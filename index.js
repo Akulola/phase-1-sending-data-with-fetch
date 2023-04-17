aa9161fe1cf05340517f4864ace4f41b86f36e08
Angela <angelakulola4@gmail.com>
2023-04-17 16:20:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+
+      //
+      
+      function submitData(name, email)  {
+        const userData = {
+          name: "Steve",
+          email: "steve@steve.com",};
+
+        const configObject = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(userData),
+        };
+        fetch("http://localhost:3000/users", configObject)
+          .then((res) => res.json())
+          .then((userData) => {
+            console.log(userData);
+            const id = userData.id;
+            document.body.innerHTML += id;
+          })
+          .catch((err) => console.log(err))
+      }
+      submitData()
